6c368ab363a1b441a45fb5ae1acb89a39e7233d0
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-03-08 17:02:46 +0000

    Dont need to install deps

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ sh -c "echo $*"
 set -e 
 export COVERALLS_REPO_TOKEN=$REPO_TOKEN
 export COVERALLS_SERVICE_NAME="GitHub Action"
-sh -c "npm i && npm run coverage && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js"
+sh -c "npm run coverage && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js"
