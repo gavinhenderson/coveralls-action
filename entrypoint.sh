b3b52f835b502027e4310d4289704c02e985939b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-03-08 16:00:39 +0000

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,4 @@
 
 # Exit if any subcommand fails
 set -e 
-
-# Setup node modules
-setup="NODE_ENV=development npm install &&"
-
-echo "## Running Jest"
-args=$@
-sh -c "$setup ./node_modules/.bin/jest $args"
+sh -c "npm i && npm run coverage | ./node_modules/coveralls/bin/coveralls.js"
