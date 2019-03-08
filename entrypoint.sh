fe32ea798c1be841921ce65d290b1c717f508258
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-03-08 16:08:05 +0000

    Added env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 # Exit if any subcommand fails
 set -e 
+export COVERALLS_REPO_TOKEN=$REPO_TOKEN
+export COVERALLS_SERVICE_NAME="GitHub Action"
 sh -c "npm i && npm run coverage && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage"
