db310bf91e3a1628ea4129b85ca65f5716278a70
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-03-08 16:06:36 +0000

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # Exit if any subcommand fails
 set -e 
-sh -c "npm i && npm run coverage | ./node_modules/coveralls/bin/coveralls.js"
+sh -c "npm i && npm run coverage && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage"
